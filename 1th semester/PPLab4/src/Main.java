647a3582efb2a2b5f5f0bb223ddade67e2c8efe6
RestlessAnchoret <et.anchoret@gmail.com>
2016-12-05 21:37:27 +0300

    Comments were added.

--- 1th semester/PPLab4/src/Main.java
+++ 1th semester/PPLab4/src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Random;
 
 public class Main {
@@ -6,7 +5,10 @@ public class Main {
     private static final int NUMBERS_BOUND = 100_000;
     
     public static void main(String[] args) {
+        // Цикл с количеством элементов в сортируемом массиве
         for (int arrayLength: new int[]{100_000, 1_000_000, 10_000_000}) {
+            // Цикл с количеством потоков для распараллеливания
+            // null - означает, что количество потоков нужно определить автоматически
             for (Integer threads: new Integer[]{1, 2, 4, 8, null}) {
                 test(arrayLength, threads);
             }
@@ -14,6 +16,7 @@ public class Main {
         }
     }
     
+    // Тестирование сортировки слиянием с заданной длиной массива и количеством потоков
     private static void test(int arrayLength, Integer threads) {
         System.out.println("Array length: " + arrayLength + "; threads quantity: " + threads);
         int[] array = generateArray(arrayLength, NUMBERS_BOUND);
@@ -29,6 +32,7 @@ public class Main {
         System.out.println("Time: " + (finish - start) + " milliseconds. Array is sorted: " + arrayIsSorted);
     }
     
+    // Генерация массива
     private static int[] generateArray(int n, int bound) {
         int[] array = new int[n];
         Random random = new Random();
@@ -38,6 +42,7 @@ public class Main {
         return array;
     }
     
+    // Автоматическая проверка того, отсортирован массив или нет
     private static boolean checkArrayIsSorted(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
--- 1th semester/PPLab4/src/MergeSort.java
+++ 1th semester/PPLab4/src/MergeSort.java
@@ -2,42 +2,57 @@ public class MergeSort {
 
     private static final int MIN_ELEMENTS_PER_THREAD = 1000;
     
+    // API-метод для сортировки массива с автоматическим определением оптимального
+    // количества потоков для распараллеливания
     public void mergeSort(int[] array) {
         int threads = countOptimalThreadsQuantity(array.length);
         mergeSort(array, threads);
     }
     
+    // API-метод для сортировки массива в заданном количестве потоков
     public void mergeSort(int[] array, int threads) {
         int[] extraArray = new int[array.length];
         mergeSort(array, extraArray, 0, array.length - 1, threads);
     }
     
+    // Сортировка слиянием участка массива в нескольких потоках
     private void mergeSort(int[] array, int[] extraArray, int from, int to, int threads) {
         if (from == to) {
             return;
         }
         int mid = (from + to) / 2;
         if (threads == 1) {
+            // Если нужно выполнить сортировку в одном потоке, выполняем её в текущем потоке целиком
             mergeSort(array, extraArray, from, mid, threads);
             mergeSort(array, extraArray, mid + 1, to, threads);
             merge(array, extraArray, from, mid, to);
         } else {
+            // Определяем количество потоков для сортировки правой и левой части участка массива
             int threadsForLeftPart = threads / 2;
             int threadsForRightPart = threads - threadsForLeftPart;
+            
+            // Запускаем в отдельном потоке сортировку левой части участка массива
             Runnable sortLeftPartTask = () -> mergeSort(array, extraArray, from, mid, threadsForLeftPart);
             Thread sortLeftPartThread = new Thread(sortLeftPartTask);
             sortLeftPartThread.start();
+            
+            // Выполняем в текущем потоке сортировку правой части участка массива
             mergeSort(array, extraArray, mid + 1, to, threadsForRightPart);
+            
+            // Дожидаемся окончания сортировки левой части участка массива
             try {
                 sortLeftPartThread.join();
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
                 throw new RuntimeException(ex);
             }
+            
+            // Сливаем отсортированные участки массива в текущем потоке
             merge(array, extraArray, from, mid, to);
         }
     }
     
+    // Слияние двух отсортированных участков массива
     private void merge(int[] array, int[] extraArray, int from, int mid, int to) {
         for (int i = from; i <= to; i++) {
             extraArray[i] = array[i];
