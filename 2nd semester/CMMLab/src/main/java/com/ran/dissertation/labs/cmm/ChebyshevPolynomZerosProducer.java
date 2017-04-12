9b4b60f3d41b3b3f936fb399a403f6cee189f84a
RestlessAnchoret <et.anchoret@gmail.com>
2017-04-12 23:57:58 +0300

    LegendrePolynomsFactory and ChebyshevPolynomZerosProducer classes were implemented.

--- 2nd semester/CMMLab/src/main/java/com/ran/dissertation/labs/cmm/ChebyshevPolynomZerosProducer.java
+++ 2nd semester/CMMLab/src/main/java/com/ran/dissertation/labs/cmm/ChebyshevPolynomZerosProducer.java
@@ -3,7 +3,11 @@ package com.ran.dissertation.labs.cmm;
 public class ChebyshevPolynomZerosProducer {
 
     public double[] produceZeros(int n) {
-        return null;
+        double[] zeros = new double[n];
+        for (int k = 0; k < n; k++) {
+            zeros[k] = Math.cos((2.0 * k + 1.0) / (2.0 * n) * Math.PI);
+        }
+        return zeros;
     }
 
 }
--- 2nd semester/CMMLab/src/main/java/com/ran/dissertation/labs/cmm/LegendrePolynomsFactory.java
+++ 2nd semester/CMMLab/src/main/java/com/ran/dissertation/labs/cmm/LegendrePolynomsFactory.java
@@ -1,11 +1,28 @@
 package com.ran.dissertation.labs.cmm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.DoubleFunction;
 
 public class LegendrePolynomsFactory {
 
+    private static List<DoubleFunction<Double>> polynoms = new ArrayList<>(6);
+
+    static {
+        polynoms.add(t -> 1.0);
+        polynoms.add(t -> t);
+        polynoms.add(t -> t * t * 1.5 - 0.5);
+        polynoms.add(t -> t * t * t * 2.5 - t * 1.5);
+        polynoms.add(t -> t * t * t * t * 35.0 / 8.0 - t * t * 15.0 / 4.0 + 3.0 / 8.0);
+        polynoms.add(t -> t * t * t * t * t * 63.0 / 8.0 - t * t * t * 35.0 / 4.0 + t * 15.0 / 8.0);
+    }
+
     public DoubleFunction<Double> getLegendrePolynomOfDegree(int degree) {
-        return null;
+        if (degree < polynoms.size()) {
+            return polynoms.get(degree);
+        } else {
+            return null;
+        }
     }
 
 }
