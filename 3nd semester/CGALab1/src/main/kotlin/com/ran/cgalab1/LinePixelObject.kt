7dcf1c747b1ec939c99bbca3330123fe8814f893
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-10-18 22:45:40 +0300

    Line drawing was implemented in CGALab1 project.

--- 3nd semester/CGALab1/src/main/kotlin/com/ran/cgalab1/LinePixelObject.kt
+++ 3nd semester/CGALab1/src/main/kotlin/com/ran/cgalab1/LinePixelObject.kt
@@ -8,8 +8,44 @@ class LinePixelObject(
 ) : PixelObject(firstPixel, secondPixel) {
 
     override fun draw(delegate: PixelRenderingDelegate) {
-        delegate.setPixel(firstPixel.x, firstPixel.y)
-        delegate.setPixel(secondPixel.x, secondPixel.y)
+        var x = firstPixel.x
+        var y = firstPixel.y
+
+        val deltaX = Math.abs(secondPixel.x - firstPixel.x)
+        val deltaY = Math.abs(secondPixel.y - firstPixel.y)
+
+        val signX = Math.signum((secondPixel.x - firstPixel.x).toDouble()).toInt()
+        val signY = Math.signum((secondPixel.y - firstPixel.y).toDouble()).toInt()
+
+        var h = 0
+        var eLessThenHalf = true
+        delegate.setPixel(x, y)
+
+        if (deltaY < deltaX) {
+            for (k in 1..deltaX) {
+                if (!eLessThenHalf) {
+                    h++
+                }
+                eLessThenHalf = (2 * k * deltaY - (2 * h + 1) * deltaX <= 0)
+                x += signX
+                if (!eLessThenHalf) {
+                    y += signY
+                }
+                delegate.setPixel(x, y)
+            }
+        } else {
+            for (k in 1..deltaY) {
+                if (!eLessThenHalf) {
+                    h++
+                }
+                eLessThenHalf = (2 * k * deltaX - (2 * h + 1) * deltaY <= 0)
+                y += signY
+                if (!eLessThenHalf) {
+                    x += signX
+                }
+                delegate.setPixel(x, y)
+            }
+        }
     }
 
 }
