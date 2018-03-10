6f8952b16d7e167f54834066fa9cfc578ccaad4a
Ilya Kuznetsov <et.anchoret@gmail.com>
2018-03-11 01:30:06 +0300

    Watsu method was implemented in ACGLab1 project.

--- 4th semester/ACGLab1/src/main/java/com/ran/acglab1/Algorithms.java
+++ 4th semester/ACGLab1/src/main/java/com/ran/acglab1/Algorithms.java
@@ -85,7 +85,74 @@ public class Algorithms {
     public static Result getBinaryImageByWatsuMethod(BufferedImage sourceImage) {
         int w = sourceImage.getWidth(null);
         int h = sourceImage.getHeight(null);
-        return new Result(sourceImage, 0);
+        int gmin = 255;
+        int gmax = 0;
+        int[] n = new int[256];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                int rgb = sourceImage.getRGB(j, i);
+                Color color = getColor(rgb);
+                int grey = color.getRed();
+                gmin = Math.min(gmin, grey);
+                gmax = Math.max(gmax, grey);
+                n[grey]++;
+            }
+        }
+        double p[] = new double[256];
+        for (int i = 0; i < 256; i++) {
+            p[i] = (double)n[i] / (double)(w * h);
+        }
+        double maxSigma = -Double.MAX_VALUE;
+        int foundT = -1;
+        for (int t = 1; t <= gmax; t++) {
+            double w0 = countW0(t, p);
+            double w1 = countW1(t, gmax, p);
+            double m0 = countM0(t, w0, p);
+            double m1 = countM1(t, gmax, w1, p);
+            double sigma = countSigma(w0, w1, m0, m1);
+            if (sigma > maxSigma) {
+                maxSigma = sigma;
+                foundT = t;
+            }
+        }
+        BufferedImage image = buildImageForBound(sourceImage, foundT);
+        return new Result(image, foundT);
+    }
+    
+    private static double countW0(int t, double[] p) {
+        double result = 0.0;
+        for (int i = 1; i <= t; i++) {
+            result += p[i];
+        }
+        return result;
+    }
+    
+    private static double countW1(int t, int gmax, double[] p) {
+        double result = 0.0;
+        for (int i = t + 1; i <= gmax; i++) {
+            result += p[i];
+        }
+        return result;
+    }
+    
+    private static double countM0(int t, double w0, double[] p) {
+        double result = 0.0;
+        for (int i = 1; i <= t; i++) {
+            result += i * p[i];
+        }
+        return result / w0;
+    }
+    
+    private static double countM1(int t, int gmax, double w1, double[] p) {
+        double result = 0.0;
+        for (int i = t + 1; i <= gmax; i++) {
+            result += i * p[i];
+        }
+        return result / w1;
+    }
+    
+    private static double countSigma(double w0, double w1, double m0, double m1) {
+        return w0 * w1 * (m1 - m0) * (m1 - m0);
     }
     
     private static Color getColor(int rgb) {
